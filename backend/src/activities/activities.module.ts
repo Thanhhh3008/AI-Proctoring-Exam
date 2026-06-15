e5942b4f5fa9947961ac49c3bad2db7173a05c35
Thanhhh3008 <nthanh30082004@gmail.com>
2026-06-15 21:35:51 +0700

    fix: import UploadModule in ActivitiesModule

--- backend/src/activities/activities.module.ts
+++ backend/src/activities/activities.module.ts
@@ -3,8 +3,10 @@ import { ActivitiesService } from './activities.service';
 import { ActivitiesController } from './activities.controller';
 
 import { PrismaService } from '../shared/prisma/prisma.service'; 
+import { UploadModule } from '../upload/upload.module';
 
 @Module({
+  imports: [UploadModule],
   controllers: [ActivitiesController],
   providers: [ActivitiesService, PrismaService],
 })
